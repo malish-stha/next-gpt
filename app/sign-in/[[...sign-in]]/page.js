b79b1c4ed9642a9168256a9db133dc3cd83021fa
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-14 17:04:39 +0545

    Add ClerkProvider to RootLayout.

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,7 @@
+import { SignIn } from "@clerk/nextjs";
+
+const SignInPage = () => {
+  return <div>page</div>;
+};
+
+export default SignInPage;
---
+++ app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return <div>Sign Up</div>;
+};
+
+export default SignUpPage;
