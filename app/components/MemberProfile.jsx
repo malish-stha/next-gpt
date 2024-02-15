6b61099985c184ec6aa8aefcf5e17ff0adb97b20
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-15 05:47:04 +0545

    Refactor MemberProfile and NavLinks components.

--- app/components/MemberProfile.jsx
+++ app/components/MemberProfile.jsx
@@ -1,5 +1,14 @@
-const MemberProfile = () => {
-  return <div>MemberProfile</div>;
+import { UserButton, auth, currentUser } from "@clerk/nextjs";
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+  // const { userId } = auth();
+  return (
+    <div className="px-4 flex items-center gap-2">
+      <UserButton affterSignOutUrl="/" />
+      <p>{user.emailAddresses[0].emailAddress}</p>
+    </div>
+  );
 };
 
 export default MemberProfile;
--- app/components/NavLinks.jsx
+++ app/components/NavLinks.jsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
+const links = [
+  { href: "/chat", label: "chat" },
+  { href: "/tours", label: "tours" },
+  { href: "/tours/new-tour", label: "new tour" },
+  { href: "/profile", label: "profile" },
+];
+
 const NavLinks = () => {
-  return <div>NavLinks</div>;
+  return (
+    <ul className="menu text-base-content">
+      {links.map((link) => {
+        return (
+          <li key={link.href}>
+            <Link href={link.href} className="capitalize">
+              {link.label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavLinks;
