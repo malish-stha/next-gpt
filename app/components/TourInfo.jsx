c2cbd25ff5060538a96662a8656faf9a0ed891b5
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-15 19:15:19 +0545

    Update dependencies and TourInfo component.

--- app/components/TourInfo.jsx
+++ app/components/TourInfo.jsx
@@ -1,5 +1,20 @@
 const TourInfo = ({ tour }) => {
-  console.log(tour);
+  const { title, description, stops } = tour;
+  return (
+    <div className="max-w-2xl">
+      <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+      <p className="loading-loose mb-6">{description}</p>
+      <ul>
+        {stops.map((stop) => {
+          return (
+            <li key={stop} className="mb-4 bg-base-100 p-4 rounded-xl">
+              <p>{stop}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
   return <div>TourInfo</div>;
 };
 
