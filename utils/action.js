d43eca39a8825d72fba2ff0af06a042a10cbc824
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-15 07:38:36 +0545

    Add OpenAI integration for chat response generation.

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,22 @@
 "use server";
 
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  api_key: process.env.OPENAI_API_KEY,
+});
+
 export const generateChatResponse = async (chatMessage) => {
-  console.log(chatMessage);
+  const response = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "you are a helpful assistant" },
+      { role: "user", content: chatMessage },
+    ],
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+  });
+  console.log(response.choices[0].message);
+  console.log(response);
+
   return "awesome";
 };
