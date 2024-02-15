ece69c9e44a44ed97179fa857ee754b82cdcfe56
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-15 05:56:03 +0545

    Update SidebarHeader and ThemeToggle components.

--- app/components/SidebarHeader.jsx
+++ app/components/SidebarHeader.jsx
@@ -5,7 +5,7 @@ const SidebarHeader = () => {
   return (
     <div className="flex items-center mb-4 gap-4 px-4">
       <SiOpenaigym className="w-10 h-10 text-primary" />
-      <h2 className="text-xl font-extrabold text-primary">NEXTGPT</h2>
+      <h2 className="text-xl font-extrabold text-primary mr-auto">NEXTGPT</h2>
       <ThemeToggle />
     </div>
   );
--- app/components/ThemeToggle.jsx
+++ app/components/ThemeToggle.jsx
@@ -1,4 +1,27 @@
+"use client";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import { useState } from "react";
+
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
 const ThemeToggle = () => {
-  return <button className="btn btn-primary btn-sm">ThemeToggle</button>;
+  const [theme, setTheme] = useState(themes.winter);
+
+  const toggleTheme = () => {
+    const newTheme = theme === themes.winter ? themes.dracula : themes.winter;
+    document.documentElement.setAttribute("data-theme", newTheme);
+    setTheme(newTheme);
+  };
+  return (
+    <button onClick={toggleTheme} className="btn btn-sm btn-outline">
+      {theme === "winter" ? (
+        <BsMoonFill className="h-4 w-4" />
+      ) : (
+        <BsSunFill className="h-4 w-4" />
+      )}
+    </button>
+  );
 };
 export default ThemeToggle;
