f0a6b421e2d7ded9d80164d1cfc8967d49d46f59
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-16 17:06:18 +0545

    Refactor ToursPage component and add search functionality.

--- app/(dashboard)/tours/page.js
+++ app/(dashboard)/tours/page.js
@@ -10,7 +10,7 @@ const AllToursPage = async () => {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["tasks"],
+    queryKey: ["tours", ""],
     queryFn: () => getAllTours(),
   });
   return (
--- app/components/ToursPage.jsx
+++ app/components/ToursPage.jsx
@@ -3,14 +3,36 @@
 import { getAllTours } from "@/utils/action";
 import { useQuery } from "@tanstack/react-query";
 import ToursList from "./ToursList";
+import { useState } from "react";
 
 const ToursPage = () => {
+  const [searchValue, setSearchValue] = useState("");
   const { data, isPending } = useQuery({
-    queryKey: ["tasks"],
-    queryFn: () => getAllTours(),
+    queryKey: ["tours", searchValue],
+    queryFn: () => getAllTours(searchValue),
   });
   return (
     <>
+      <form className="max-w-lg mb-12">
+        <div className="join w-full">
+          <input
+            type="text"
+            placeholder="enter city or country...."
+            className="input input-bordered join-item w-full"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            required
+          />
+          <button
+            className="btn btn-primary join-item"
+            type="button"
+            disabled={isPending}
+            onClick={() => setSearchValue("")}
+          >
+            {isPending ? "please wait" : "reset"}
+          </button>
+        </div>
+      </form>
       {isPending ? (
         <span className="loading"></span>
       ) : (
